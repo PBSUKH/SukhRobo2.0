cd8a33780a90c91718a998d592ab895048957628
badmunda011 <139567638+badmunda011@users.noreply.github.com>
2023-10-30 08:50:40 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -7,9 +7,9 @@ class Config(object):
     API_ID = "" # integer value, dont use ""
     API_HASH = ""
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 6566179661 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 6494633110 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
-    SUPPORT_CHAT = "https://t.me/THE_DRAMA_CLUB_01"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "its_Punjabi_Chat_Group"  # Your own group for support, do not add the @
     START_IMG = ""
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     MONGO_DB_URI= ""
